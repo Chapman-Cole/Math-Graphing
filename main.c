ec3b10520860a694aba64663a972b886490892f8
cole chapman <colechapman924@gmail.com>
2025-08-28 20:40:56 -0700

    Barebones Completed

--- main.c
+++ main.c
@@ -1,6 +1,62 @@
 #include <stdio.h>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include <stdlib.h>
+
+#define true 1
+#define false 0
+
+//Note: gdi32.lib is a dependency of glfw, so you have to remember to link against it when using opengl
+//This goes for either the libglfw3.a or glfw3.lib files (the former being compiled with mingw and the latter being compiled with visual studio)
+//gcc main.c C:\ProgrammingUtils\OpenGL\lib\glad.c -I"C:\ProgrammingUtils\OpenGL\includes" -LC:\ProgrammingUtils\OpenGL\lib -l:libglfw3.a -lgdi32 -o main
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+void processInput(GLFWwindow* window);
 
 int main(void) {
-    printf("Hello World!\n");
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL!", NULL, NULL);
+    if (window == NULL) {
+        printf("Failed to create GLFW window\n");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        printf("Failed to initialize GLAD\n");
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
